4dd7c34fe40ef68c13fd0430ec876421b5ebbc3a
Lucas Copque <lucas.copque@mobiauto.com.br>
2025-08-11 18:21:55 -0300

    init project

---
+++ src/main/java/br/com/mindmaster/shedlock/SpringbootShedlockExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.mindmaster.shedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShedlockExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShedlockExampleApplication.class, args);
+    }
+
+}
